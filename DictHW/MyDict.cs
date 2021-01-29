95610723429a78f40a5c258e6e8b11afa3a7125d
huaydin <hurkanaydin@gmail.com>
2021-01-30 01:23:18 +0300

    Add project files.

---
+++ DictHW/MyDict.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictHW
+{
+    class MyDict <Tkeys, Tvalues>
+    {
+        Tkeys[] keys;
+        Tvalues[] values;
+
+        public MyDict()
+        {
+            keys = new Tkeys[0];
+            values = new Tvalues[0];
+        }
+
+        public void Add(Tkeys key, Tvalues value)
+        {
+            Tkeys[] tempkeys = keys;
+            Tvalues[] tempvalues = values;
+
+            keys = new Tkeys[keys.Length + 1];
+            values = new Tvalues[values.Length + 1];
+
+            for (int i = 0; i < tempkeys.Length; i++)
+            {
+                keys[i] = tempkeys[i];
+            }
+
+            for (int i = 0; i < tempvalues.Length; i++)
+            {
+                values[i] = tempvalues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void Items()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Ders kodu: " + keys[i] + "Ders Adı: " + values[i]);
+                Console.WriteLine("----Ders eklenmiştir!----");
+            }
+        }
+    }
+
+
+}
---
+++ DictHW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<int, string> CourseList = new MyDict<int, string>();
+            CourseList.Add(101, "Fundementals of CS");
+            CourseList.Add(102, "C#");
+
+            CourseList.Items();
+
+        }
+    }
+}
